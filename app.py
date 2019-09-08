f30a1cfdef4b767529c3de47c2a5106f30b88126
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2019-09-08 20:23:25 +0530

    Changes

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return "<h1>Flair Prediction Model As Microservice (API)</h1>"
 
+@app.route('/predict', methods=['GET'])
+def predict():
+    if 'post' in request.args:
+        Url = request.args['post']
+    else:
+        return jsonify("Error : No Post URL provided !")
+    
+    result = ["Results"]
+    return jsonify(list(result))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
